182e2d79e058f13d40370fafc1b6f6c587e9fb62
OHJINSEONG <ojseong0828@naver.com>
2024-04-03 14:02:49 +0900

    변경사항 임시저장

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import React from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { apiService } from './services/ApiService';
 
@@ -13,7 +13,7 @@ const accessToken = JSON.parse(data);
 apiService.setAccessToken(accessToken);
 
 root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
         <App />
-    </HashRouter>
+    </BrowserRouter>
 );
